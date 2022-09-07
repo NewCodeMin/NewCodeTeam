e059076f8afcf054c349b3a47a6512c6d0d1c6eb
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-07 02:48:32 -0500

    Chore: Controladores rest de movimientos de empresa

--- src/main/java/com/NewCodeTeam/Comercializadora/rest/EnterpriseRest.java
+++ src/main/java/com/NewCodeTeam/Comercializadora/rest/EnterpriseRest.java
@@ -2,6 +2,7 @@ package com.NewCodeTeam.Comercializadora.rest;
 
 import com.NewCodeTeam.Comercializadora.Service.EnterpriseService;
 import com.NewCodeTeam.Comercializadora.model.Enterprise;
+import com.NewCodeTeam.Comercializadora.model.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -55,6 +56,41 @@ public class EnterpriseRest {
             return "Se pudo eliminar correctamente la empresa con id "+id;
         }else{
             return "No se puedo eliminar correctamente la empresa con id "+id;
+        }
+    }
+
+    @GetMapping ("enterprises/{id}/movements")
+    private ResponseEntity<List<Transaction>> getAllEstadosByPais (@PathVariable("id") Long id){
+        return ResponseEntity.ok(enterpriseService.findMovimentsEnterpriseByIdEnterprise(id));
+    }
+
+    @PostMapping("/enterprises/{id}/movements")
+    public  ResponseEntity<Transaction> saveMovementEnterprise (@PathVariable("id") Long id,@RequestBody Transaction transaction){
+        try {
+            Transaction transactionSave = enterpriseService.saveTransaction(transaction);
+            return ResponseEntity.created(new URI("/api/enterprises/"+id+"/movements"+ transactionSave.getId())).body(transactionSave);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @PatchMapping("/enterprises/{id}/movements")
+    public  ResponseEntity<Transaction> updateMovementEnterprise (@PathVariable("id") Long id,@RequestBody Transaction transaction){
+        try {
+            Transaction transactionSave = enterpriseService.saveTransaction(transaction);
+            return ResponseEntity.created(new URI("/api/enterprises/"+id+"/movements"+ transactionSave.getId())).body(transactionSave);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
+
+    @DeleteMapping(value = "/enterprises/{idEnterprise}/movements/{id}")
+    public  String deleteMovementEnterprise (@PathVariable("id") Long id){
+        boolean answer=enterpriseService.deleteMovementById(id);
+        if (answer){
+            return "Se pudo eliminar correctamente el movimiento de la empresa";
+        }else{
+            return "No se puedo eliminar correctamente el movimiento de la empresa";
         }
     }
 }
