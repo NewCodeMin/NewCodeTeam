517f5b3eded2b7048a2debf3c95834065e08366d
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-21 22:27:08 -0500

    chore: vista login y logout

--- src/main/java/com/NewCodeTeam/Comercializadora/security/SecurityConfiguration.java
+++ src/main/java/com/NewCodeTeam/Comercializadora/security/SecurityConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
 
@@ -35,10 +36,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/login/**").permitAll()
                 .antMatchers("/api/enterprises/**").hasRole("ADMIN")
-                .antMatchers("/api/employees/**").hasRole("USER")
+                .antMatchers("/api/employees/**").hasRole("ADMIN")
                 .and().formLogin().successHandler(customSuccessHandler)
+                .loginPage("/login").permitAll()
                 .and().exceptionHandling().accessDeniedPage("/api/Denegado")
-                .and().logout().permitAll();
+                .and().logout()
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login?logout")
+                .permitAll();
     }
 }
