e526d8f04712da3f436f8f960314bb915417017b
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-23 17:58:17 -0500

    chore: insertar imagen

--- src/main/java/com/NewCodeTeam/Comercializadora/rest/ProfileController.java
+++ src/main/java/com/NewCodeTeam/Comercializadora/rest/ProfileController.java
@@ -8,6 +8,7 @@ import com.NewCodeTeam.Comercializadora.model.Employee;
 import com.NewCodeTeam.Comercializadora.model.Enterprise;
 import com.NewCodeTeam.Comercializadora.model.Profile;
 import com.NewCodeTeam.Comercializadora.model.enumeration.EnumRoleName;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,13 +17,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
 @Controller
 @RequestMapping("/api")
@@ -48,12 +51,36 @@ public class ProfileController {
         return "newProfile";
     }
 
+    public String nameAleatorio (){
+        char n;
+        Random rnd = new Random();
+        String cadena = new String();
+        for (int i=0; i < 10 ; i++) {
+            n = (char)(rnd.nextDouble() * 26.0 + 65.0 );
+            cadena += n; }
+        return  cadena;
+    }
+
    @PostMapping("/saveProfile")
-    public  String saveProfile (@ModelAttribute("profile") Profile profile, RedirectAttributes redirectAttributes){
+    public  String saveProfile (@ModelAttribute("profile") Profile profile, @RequestParam("file") MultipartFile image, RedirectAttributes redirectAttributes){
+        String extension = FilenameUtils.getExtension(image.getOriginalFilename());
+        String nameAle = nameAleatorio() + "."+extension;
+       if(!image.isEmpty()){
+           Path directoryImages = Paths.get("src//main//resources//static/imgUs");
+           String rutaAbsoluta = directoryImages.toFile().getAbsolutePath();
+           try {
+               byte[] bytesImg = image.getBytes();
+               Path rutaCompleta = Paths.get(rutaAbsoluta + "//"+ nameAle);
+               Files.write(rutaCompleta, bytesImg);
+           }catch (IOException e){
+               e.printStackTrace();
+           }
+       }
        Authentication auth= SecurityContextHolder.getContext().getAuthentication();
        String email=auth.getName();
         try {
             Employee employee = employeeService.findByEmail(email);
+            profile.setImage(nameAle);
             if(employee.getProfile() != null){
                 profile.setUser(employee);
                 profile.setId(employee.getProfile().getId());
